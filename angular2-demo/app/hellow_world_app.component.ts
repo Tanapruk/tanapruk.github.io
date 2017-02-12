98f2293c083d6cfb5c61cdba9671cf7f12ca72e0
Tanapruk <trusthaha@gmail.com>
2017-02-12 16:43:57 +0700

    Rename to index.html so that we can npm start

--- angular2-demo/app/hellow_world_app.component.ts
+++ angular2-demo/app/hellow_world_app.component.ts
@@ -2,6 +2,7 @@
  * Created by trusttanapruk on 2/12/2017.
  */
 
+//A Module
 import {Component, View} from "angular2/core";
 
 //create custom html tag
@@ -11,10 +12,10 @@ import {Component, View} from "angular2/core";
 
 //inside the custom tag is this code
 @View({
-    template : '<h2>Hello World!!</h2>'
+    template: '<h2>Hello World!!{{name}}</h2>'
 })
 
 //Other files will known this class as this
 export class MyHelloWorldClass {
-
+    name: 'Trust Tanapruk'
 }
